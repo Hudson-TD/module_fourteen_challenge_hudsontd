ef71c3ed9276fdd276c0ea1c9139efba33680837
Tyler Hudson <tylerhudson96@gmail.com>
2022-10-08 17:48:38 -0400

    get post by id homepage route created

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -21,8 +21,10 @@ router.get('/', (req, res) => {
       },
     ],
   })
-    .then((returnedPostData) => {
-      const posts = returnedPostData.map((post) => post.get({ plain: true }));
+    .then((returnedPostsData) => {
+      const posts = returnedPostsData.map((posts) =>
+        posts.get({ plain: true })
+      );
 
       res.render('homepage', {
         posts,
@@ -32,6 +34,39 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'post_text', 'title', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+    ],
+  })
+  .then(returnedPostData => {
+    if(!returnedPostData) {
+      res.status(404).json({message: 'Could not find a post with this id'});
+      return;
+    }
+    const singlePost = returnedPostData.get({plain: true});
+    res.render('singlePost', {
+      singlePost
+    })
+  })
 });
 
 module.exports = router;
