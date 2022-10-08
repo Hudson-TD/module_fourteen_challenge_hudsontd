17ee014f9e2e3ca10ae0ee9227811009b97ff770
Tyler Hudson <tylerhudson96@gmail.com>
2022-10-08 17:16:08 -0400

    homepage rendering correctly when server is started

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -20,7 +20,18 @@ router.get('/', (req, res) => {
         },
       },
     ],
-  });
+  })
+    .then((returnedPostData) => {
+      const posts = returnedPostData.map((post) => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
