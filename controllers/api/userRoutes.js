1b5643a8471f780f0729bc4c8ac82d496cba9cb1
Tyler Hudson <tylerhudson96@gmail.com>
2022-10-09 11:12:42 -0400

    user api get routes implemented and working

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_text', 'created_at'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title'],
+        },
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+      },
+    ],
+  })
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
